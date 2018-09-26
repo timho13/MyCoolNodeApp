82402dcb98f3514fb5cfd5d073bf3dc58ae67e0b
Tim Howarth <tim.howarth13@gmail.com>
2018-09-26 04:29:37 +0000

    app.js personalised

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+res.end('<h2>This is just a starter app, next is a Python app</h2><h1>Hello Worldy</h1>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
