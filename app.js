1c9655f19fa5471bca868af14824819b0020ae75
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-27 04:46:15 +0100

    Update app.js
    Reverted to long string of html. Line feed techniques will be explored next.

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello Worldy</h1>
-        <h5>Thanks to Rohit Ramname for the initial instruction to get something showing on a publicly available page outside my home network. Available at: https://medium.freecodecamp.org/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe</h5>');
+res.end('<h1>Hello Worldy</h1><h5>Thanks to Rohit Ramname for the initial instruction to get something showing on a publicly available page outside my home network. Available at: https://medium.freecodecamp.org/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe</h5>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
