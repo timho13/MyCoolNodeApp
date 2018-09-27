b78d0abc25bf5fcd7cfabc1f2160556d9bea9354
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-27 05:18:48 +0100

    Update app.js
    Added </br> to res.write(),hopefully to add a line feed.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
+res.write('This page is a sandbox to try out new ideas.'+'</br>');
 res.write("The date and time will go here");
-res.write("This page is a sandbox to try out new ideas.");
 res.end('<h1>Hello Worldy</h1><h5>Thanks to Rohit Ramname for the initial instruction to get something showing on a publicly available page outside my home network. Available at: https://medium.freecodecamp.org/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe</h5>');
 });
 server.listen(port,() => {
