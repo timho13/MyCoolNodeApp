dee5f35a111e7f67e333a151d19d6320cc3c308f
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-27 05:36:24 +0100

    Update app.js
    Hyperlink added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.write('This page is a sandbox to try out new ideas.'+'</br>');
 res.write("The date and time will go here");
+  
+res.write('Find the code for this page on ' + <a href="https://medium.freecodecamp.org/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe">here</a>);
+
+  
 res.end('<h1>Hello Worldy</h1><h5>Thanks to Rohit Ramname for the initial instruction to get something showing on a publicly available page outside my home network. Available at: https://medium.freecodecamp.org/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe</h5>');
 });
 server.listen(port,() => {
