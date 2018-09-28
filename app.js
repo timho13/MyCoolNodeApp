2cd15edf965fb7f7fa6d5a9fb4242f6fd0482da0
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 00:44:22 +0100

    Update app.js
    Only second file is now queried.

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ const http = require('http');
 var fs = require('fs');
 const port=process.env.PORT || 3000
 const server = http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-});
-const server = http.createServer(function (req, res) {
   fs.readFile('demofile2.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
