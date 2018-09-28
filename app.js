c63d1c012f5d7e051d0e911d47ee2d2b4ba6ebb2
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 00:51:59 +0100

    Update app.js
    Variable name changed from ‘data’ to ‘data2’ in 2 places.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const http = require('http');
 var fs = require('fs');
 const port=process.env.PORT || 3000
 const server = http.createServer(function (req, res) {
-  fs.readFile('demofile2.html', function(err, data) {
+  fs.readFile('demofile2.html', function(err, data2) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
+    res.write(data2);
     res.end();
   });
 });
