2b379fdefded723df2a7c0815df3d8f95b73383e
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-28 07:42:47 +0100

    Update app.js
    Branch to test file manipulation

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const http = require('http');
-var dt = require('./utilities');
+var fs = require('fs');
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.write('This page is a sandbox to try out new ideas.'+'</br>');
-res.write('The date and time of the last refresh are: '+ dt.myDateTime() +'</br>');
-res.write('</br>');
- 
-res.write('Find the code for this page on ' + '<a href="https://github.com/timho13/MyCoolNodeApp/">Github</a>'+'</br>');
-res.write('</br>');
-res.write('Acknowledgments:'+'</br>');
-
-res.write('Thanks to Rohit Ramname for the initial instruction to get something showing on a publicly available page outside my home network. Available ' + '<a href="https://medium.freecodecamp.org/how-to-deploy-a-nodejs-app-to-heroku-from-github-without-installing-heroku-on-your-machine-433bec770efe">here</a>');
-res.write('</br>');
-res.end('<h1>Hello World</h1>');
+const server = http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
