5f9a1fb94bbf645a4994db441fc10fe30a3d7149
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 02:43:23 +0100

    Update app.js
    File reading completed before server response instance created.

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const http = require('http');
 var fs = require('fs');
 var header="<html><head></head>";
 var footer="</body></html>"
-var topdata=header;
+var data=header;
 const port=process.env.PORT || 3000
  fs.readFile('demofile1.html', function(err, data1) {
-     topdata=topdata+data1; 
+     data=data+data1; 
   });
+fs.readFile('demofile2.html', function(err, data2) {
+    data=data+data2+footer
+});
 var server = http.createServer(function (req, res) {
-  fs.readFile('demofile2.html', function(err, data2) {
-    data2=topdata+data2+footer
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data2);
-    data2=topdata;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
     res.end();
-  });
+  
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
