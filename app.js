2d10dd466d126638672368dfbb53b9e45f687320
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 02:49:39 +0100

    Update app.js
    Trial appendFile instruction added. Indentation tidied.

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ var header="<html><head></head>";
 var footer="</body></html>"
 var data=header;
 const port=process.env.PORT || 3000
- fs.readFile('demofile1.html', function(err, data1) {
-     data=data+data1; 
-  });
+fs.readFile('demofile1.html', function(err, data1) {
+  data=data+data1; 
+});
 fs.readFile('demofile2.html', function(err, data2) {
-    data=data+data2+footer
+  data=data+data2+footer
+});
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
 });
 var server = http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();  
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
