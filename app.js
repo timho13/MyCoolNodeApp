8f6846fd70e98eb383df8a3d19e2f8c89f99c54c
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 02:14:24 +0100

    Update app.js
    Footer added, header completed, data reset after rendering

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const http = require('http');
 var fs = require('fs');
-var header="<html>";
+var header="<html><head></head>";
+var footer="</body></html>"
 var data=header;
 const port=process.env.PORT || 3000
  fs.readFile('demofile1.html', function(err, data1) {
-    data=data+data1
-    
+    data=data+data1    
   });
-
 var server = http.createServer(function (req, res) {
   fs.readFile('demofile2.html', function(err, data2) {
-    data=data+data2
+    data=data+data2+footer
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
-    data=header;
+    data="";
     res.end();
   });
 });
