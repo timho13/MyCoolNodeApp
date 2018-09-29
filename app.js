8fe7cca72fe47dc28401fe8a86bbf11765364c72
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 01:14:14 +0100

    Update app.js
    Assignment of data2 from query to predeclared data variable.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 var fs = require('fs');
+var data="";
 const port=process.env.PORT || 3000
 const server = http.createServer(function (req, res) {
   fs.readFile('demofile2.html', function(err, data2) {
+    data=data2
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data2);
+    res.write(data);
     res.end();
   });
 });
