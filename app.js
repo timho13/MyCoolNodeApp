4ef83c08cb6ce7f512709c5ad366ed64679e52d9
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 01:30:18 +0100

    Update app.js
    Reset value of data before res.end()

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const server = http.createServer(function (req, res) {
     data=data+data2
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
+    data=header;
     res.end();
   });
 });
