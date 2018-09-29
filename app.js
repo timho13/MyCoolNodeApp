2a3752553a3143ff249e788e7a7bb539f3566c44
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 02:34:07 +0100

    Update app.js
    Variable topdata introduced to allow value to be reset before refresh.

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const http = require('http');
 var fs = require('fs');
 var header="<html><head></head>";
 var footer="</body></html>"
-var data=header;
+var topdata=header;
 const port=process.env.PORT || 3000
  fs.readFile('demofile1.html', function(err, data1) {
-    data=data+data1    
+     topdata=topdata+data1; 
   });
 var server = http.createServer(function (req, res) {
   fs.readFile('demofile2.html', function(err, data2) {
-    data=data+data2+footer
+    data2=topdata+data2+footer
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    data="";
+    res.write(data2);
+    data2=topdata;
     res.end();
   });
 });
