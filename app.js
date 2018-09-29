0349089aee56f79adfe9eeca45d7cfa0966c0f8d
timho13 <43578924+timho13@users.noreply.github.com>
2018-09-29 01:49:23 +0100

    Update app.js
    Contents of datafile1 read but stored in data instead of rendered.

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var header="<html>";
 var data=header;
 const port=process.env.PORT || 3000
 var server = http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data1) {
+    data=data+data1
+    
+  });
+});
+var server = http.createServer(function (req, res) {
   fs.readFile('demofile2.html', function(err, data2) {
     data=data+data2
     res.writeHead(200, {'Content-Type': 'text/html'});
